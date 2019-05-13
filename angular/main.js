a57d7bffd95e743a2445210e473b0864f2a9f645
gopaljsh <35987004+gopaljsh@users.noreply.github.com>
2019-05-13 02:21:11 -0700

    fixing http request issue

--- angular/main.js
+++ angular/main.js
@@ -1761,7 +1761,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    apiUrl: 'https://www.gopal-joshi.herokuapp.com/api'
+    apiUrl: 'http://gopal-joshi.herokuapp.com/api'
 };
 /*
  * For easier debugging in development mode, you can import the following file
