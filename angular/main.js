52a98d1e30d0ce27d3467ffe09cb34d271cca176
gopaljsh <35987004+gopaljsh@users.noreply.github.com>
2019-05-13 01:32:27 -0700

    change local url to domain

--- angular/main.js
+++ angular/main.js
@@ -1761,7 +1761,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://www.gopal-joshi.herokuapp.com/api'
 };
 /*
  * For easier debugging in development mode, you can import the following file
